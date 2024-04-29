138a3006964df678dd146f3125ac016aac13b386
Henrique Moraes <ahmdesouza@gmail.com>
2024-04-29 15:26:40 -0300

    update

--- sistemarh/src/main/java/br/com/apprh/sistemarh/dataConfiguration.java
+++ sistemarh/src/main/java/br/com/apprh/sistemarh/dataConfiguration.java
@@ -2,9 +2,14 @@ package br.com.apprh.sistemarh;
 
 import javax.sql.DataSource;
 
+import org.aspectj.internal.lang.annotation.ajcDeclareParents;
+import org.hibernate.Hibernate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 
 @Configuration
 public class dataConfiguration {
@@ -16,8 +21,21 @@ public class dataConfiguration {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/apprh?useTimezone=true&serverTimezone=UTC");
-        dataSource.getUsername("root");
-        dataSource.getPassword("admin");
-        return (DataSource) dataSource(); //verificar video com link por email para continuidade
+        dataSource.setUsername("root");
+        dataSource.setPassword("admin");
+        return dataSource;
+        
+    }
+    
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
     }
 }
+
