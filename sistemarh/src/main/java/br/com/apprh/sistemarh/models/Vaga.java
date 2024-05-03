36be4174f827b6ff5d6544b5aa360beea63c78f5
henriqueMors <ahmdesouza@gmail.com>
2024-05-03 18:29:44 -0300

    update models

---
+++ sistemarh/src/main/java/br/com/apprh/sistemarh/models/Vaga.java
@@ -0,0 +1,40 @@
+package br.com.apprh.sistemarh.models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.oneToMany;
+
+import javax.validation.constraints.NotEmpty;
+
+import jakarta.annotation.Generated;
+
+public class Vaga implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private long codigo;
+
+    @NotEmpty
+    private String nome;
+
+    @NotEmpty
+    private String descricao;
+
+    @NotEmpty
+    private String data;
+
+    @NotEmpty
+    private String salario;
+
+    
+
+
+
+}
