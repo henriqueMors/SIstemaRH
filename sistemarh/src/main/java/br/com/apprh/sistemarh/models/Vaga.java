8bfa1f52a4cb4f84017b8d8f2a11fa43944cab8c
Henrique Moraes <ahmdesouza@gmail.com>
2024-05-05 21:05:03 -0300

    Update Vaga.java

--- sistemarh/src/main/java/br/com/apprh/sistemarh/models/Vaga.java
+++ sistemarh/src/main/java/br/com/apprh/sistemarh/models/Vaga.java
@@ -7,8 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.oneToMany;
-
+import javax.persistence.OneToMany;
 
 import javax.validation.constraints.NotEmpty;
 
@@ -33,8 +32,6 @@ public class Vaga implements Serializable {
 
     @NotEmpty
     private String salario;
-
-    
 
 
 
