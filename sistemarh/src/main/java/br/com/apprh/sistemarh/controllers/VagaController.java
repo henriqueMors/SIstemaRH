e715f089cef13093e4e43e78b1c3d2669209956a
henriqueMors <ahmdesouza@gmail.com>
2024-05-07 15:06:22 -0300

    Create VagaController.java

---
+++ sistemarh/src/main/java/br/com/apprh/sistemarh/controllers/VagaController.java
@@ -0,0 +1,20 @@
+package br.com.apprh.sistemarh.controllers;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.apprh.sistemarh.models.Candidato;
+import br.com.apprh.sistemarh.models.Vaga;
+
+@Controller
+public class VagaController {
+    
+    
+}
