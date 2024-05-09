826f342e0cc15c1e13ba08fda589391489a2abb5
henriqueMors <ahmdesouza@gmail.com>
2024-05-09 15:26:58 -0300

    Update VagaRepository.java

--- sistemarh/src/main/java/br/com/apprh/sistemarh/repository/VagaRepository.java
+++ sistemarh/src/main/java/br/com/apprh/sistemarh/repository/VagaRepository.java
@@ -1,5 +1,15 @@
 package br.com.apprh.sistemarh.repository;
 
-public interface VagaRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.apprh.sistemarh.models.Vaga;
+
+public interface VagaRepository extends CrudRepository<Vaga, String> {
+
+    Vaga findByCodigo(long codigo);
+    List<Vaga> findByNome(String nome);
 
 }
