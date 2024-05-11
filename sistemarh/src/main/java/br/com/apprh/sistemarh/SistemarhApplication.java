a7b338f6c4d37eebc009a42456201a4ecedbd260
Henrique Moraes <ahmdesouza@gmail.com>
2024-05-11 00:17:27 -0300

    Update SistemarhApplication.java

--- sistemarh/src/main/java/br/com/apprh/sistemarh/SistemarhApplication.java
+++ sistemarh/src/main/java/br/com/apprh/sistemarh/SistemarhApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"br.com.apprh.sistemarh.models"})
 public class SistemarhApplication {
 
 	public static void main(String[] args) {
