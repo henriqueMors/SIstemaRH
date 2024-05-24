36b96d25f426d257225de8af643d9b5322ab0f74
henriqueMors <ahmdesouza@gmail.com>
2024-05-24 18:39:40 -0300

    update

--- sistemarh/src/main/java/br/com/apprh/sistemarh/repository/VagaRepository.java
+++ sistemarh/src/main/java/br/com/apprh/sistemarh/repository/VagaRepository.java
@@ -2,8 +2,11 @@ package br.com.apprh.sistemarh.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import br.com.apprh.sistemarh.models.Vaga;
 
+@Repository
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
     
     Vaga findByCodigo(long id);
